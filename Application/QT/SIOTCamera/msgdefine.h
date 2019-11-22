693d36fb2ed84c0cd55f8f22aa491cb810fc9927
YCH-caihui <chyangbast@yeah.net>
2019-11-23 01:18:51 +0800

    [ADD LAME]

--- Application/QT/SIOTCamera/msgdefine.h
+++ Application/QT/SIOTCamera/msgdefine.h
@@ -1,4 +1,84 @@
 #ifndef MSGDEFINE_H
 #define MSGDEFINE_H
 
+#define MJCAM_DEVICE_MAX_NUM  9
+
+#define    CONTROLLCODE_LOGINREQUEST                0    //登陆请求  (操作命令)
+#define    CONTROLLCODE_LOGINREPLY                  1    //登陆回应  (操作命令)
+#define    CONTROLLCODE_VERIFIYREQUEST              2    //验证请求  (操作命令)
+#define    CONTROLLCODE_VERIFIYREPLY                3    //验证回应  (操作命令)
+#define    CONTROLLCODE_VIDEOTRANSLATION_REQUEST    4    //视频请求  (操作命令)
+#define    CONTROLLCODE_VIDEOTRANSLATION_REPLY      5    //视频回应  (操作命令)
+#define    CONTROLLCODE_VIDEOTRANSLATION_STOP       6    //视频停止  (操作命令)
+
+#define    CONTROLLCODE_LISTENSTART_COMMAND         8    //开始音频  (操作命令)
+#define    CONTROLLCODE_LISTENSTART_REPLY           9    //音频回应  (操作命令)
+#define    CONTROLLCODE_LISTENSTOP_COMMAND          10   //停止音频  (操作命令)
+
+
+
+#define    CONTROLLCODE_SEARCH_BROADCAST_REQUEST    0   //广播请求
+#define    CONTROLLCODE_SEARCH_BROADCAST_REPLY      1   //广播回应
+
+#define    CONTROLLCODE_DECODER_CONTROLL            14   //解码器      (操作命令)
+#define    CONTROLLCODE_REPLY_ALARM                 25   //报警        (操作命令)
+#define    CONTROLLCODE_MULTIVIDEOINFOREPLY         28   //多路信息操   (操作命令)
+
+#define    CONTROLLCODE_VIDEOTRANSPORTCOMMD         0   //视频传输请   (传输命令)
+#define    CONTROLLCODE_VIDEOTRANSPORTREPLY         1   //视频传输回应   (传输命令)
+#define    CONTROLLCODE_LISTENTRANSPORTREPLY        2   //监听回应操作码      (传输命令)
+#define    CONTROLLCODE_TALKTRANSPORTREPLY          3   //对讲回应操作码      (传输命令)
+
+
+#define    CONTROLLCODE_USERDATA_REQ               32
+#define    CONTROLLCODE_USERDATA_REPLY             33
+
+#define    CONTROLLCODE_LASER_CTRL_REQ             34
+#define    CONTROLLCODE_LASER_CTRL_REPLY           35
+#define    CONTROLLCODE_AIR_QUALITY_REQ            36
+#define    CONTROLLCODE_AIR_QUALITY_REPLY          37
+#define    CONTROLLCODE_WRITE_UID_REQ              38
+#define    CONTROLLCODE_WRITE_UID_REPLY            39
+#define    CONTROLLCODE_SET_WIFI_REQ               40
+#define    CONTROLLCODE_SET_WIFI_REPLY             41
+#define    CONTROLLCODE_GET_SD_REQ                 42
+#define    CONTROLLCODE_GET_SD_REPLY               43
+
+#define    CONTROLLCODE_KEEPALIVECOMMAND            255
+
+#define    LOGINREQUESTREPLYVALUE_OK                0
+#define    LOGINREQUESTREPLYVALUE_BADACCESS         1
+
+#define     VERIFYREPLYRETURNVALUE_OK               0   //较验正确
+#define     VERIFYREPLYRETURNVALUE_USER_ERROR       1   //用户名出错
+#define     VERIFYREPLYRETURNVALUE_PASS_ERROR       5   //密码出错
+
+#define     VIDEOIREQUESTREPLY_OK                   0   //同意连接
+#define     VIDEOIREQUESTREPLY_USERFULL             2   //用户已经满
+#define     VIDEOIREQUESTREPLY_FORBIDEN             8   //禁止连接
+
+
+typedef struct CC_MessageHeader
+{
+    unsigned char messageHeader[4]; //x协议头 摄像头操作协议： "CCTC"摄像头传输协议 "CCTD"
+    short controlMask; // 操作码，区分同一协议中的不同命令
+    unsigned char reseved0;  // 保留 默认= 0；
+    unsigned char reserved1[8]; //保留
+    int commandLength;  //命令中正文的长度
+    int rewserved2; //保留
+} CC_MsgHeader;
+
+//登陆响应结构体
+typedef struct LoginRequestReply
+{
+    short           result;   //返回 0 OK,2 已经达到最大连接许可，连接将断开.
+    unsigned char   devID[13];  //返回设备id.
+    unsigned char   reserved0[4];  //保留
+    unsigned char   reserved1[4];  //保留
+    unsigned char   devVersion[4];  //摄像头系统固件版本.
+
+}CC_LoginRequestReply;
+
+
+
 #endif // MSGDEFINE_H
--- Application/QT/SIOTCamera/socketserver.cpp
+++ Application/QT/SIOTCamera/socketserver.cpp
@@ -3,11 +3,230 @@
 static int m_ListenSocket = -1;
 static int m_BindSocket = -1;
 
+
+
+int detach_thread_create(pthread_t *thread, void * start_routine, void *arg)
+{
+    pthread_attr_t attr;
+    pthread_t thread_t;
+    int ret = 0;
+
+    if(thread==NULL){
+        thread = &thread_t;
+    }
+    //初始化线程的属性
+    if(pthread_attr_init(&attr))
+    {
+        printf("pthread_attr_init fail!\n");
+        return -1;
+    }
+
+    //设置线程detachstate属性。该表示新线程是否与进程中其他线程脱离同步
+    if(pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED))
+    {//新线程不能用pthread_join()来同步，且在退出时自行释放所占用的资源。
+        printf("pthread_attr_setdetachstate fail!\n");
+        goto error;
+    }
+
+    ret = pthread_create(thread, &attr, (void *(*)(void *))start_routine, arg);
+    if(ret < 0){
+        printf("pthread_create fail!\n");
+        goto error;
+    }
+
+    //将状态改为unjoinable状态，确保资源的释放。
+   ret =  pthread_detach(thread_t);
+
+error:
+    pthread_attr_destroy(&attr);
+
+    return ret;
+}
+
+//非阻塞套接字发送
+int tcp_send(int socketFd, char * sendBuffer, unsigned int sendLen)
+{
+    if(socketFd < 0)
+    {
+        return -1;
+    }
+
+    if(sendBuffer == NULL || sendLen <= 0)
+    {
+        return -1;
+    }
+
+    fd_set write_set;
+    struct timeval tmval;
+    memset(&write_set, 0, sizeof(tmval));
+
+    int nRet = 0;
+    int sendSIze = 0;
+
+    while(sendSIze < sendLen)
+    {
+        FD_ZERO(&write_set);
+        FD_SET(socketFd, &write_set);
+
+        tmval.tv_sec = 3;
+        tmval.tv_usec = 0;
+        nRet = select(socketFd + 1, &write_set, NULL, NULL, &tmval);
+        if(nRet == 0)
+        {
+            usleep((10 * 1000));
+            continue;
+        }
+
+        if(nRet < 0 && nRet == EINTR)
+        {
+            usleep(10 * 1000);
+            continue;
+        }
+
+        if(nRet < 0)
+        {
+            return -1;
+        }
+
+        if(FD_ISSET(socketFd, &write_set))
+        {
+
+      RESENDBUFFER:
+            nRet = send(socketFd, sendBuffer + sendSIze, sendLen - sendSIze, 0);
+
+            if(nRet > 0)
+            {
+                sendLen -= nRet;
+                sendSIze += nRet;
+            }
+
+            if(nRet < 0 && errno == EINTR)
+            {
+                goto RESENDBUFFER;
+
+            }
+
+        }
+    }
+}
+
+//非阻塞套接字收
+int tcp_receive(int socketFd, char * pBuffer, unsigned int nSize)
+{
+    if(pBuffer == NULL || (nSize < 0))
+    {
+        return -1;
+    }
+
+    if(socketFd < 0)
+    {
+        return -1;
+    }
+
+
+     int ret = 0;
+     fd_set read_set;
+     struct timeval tmval;
+     memset(&tmval, 0 , sizeof(tmval));
+
+     int recvedSize = 0;
+
+
+     while(recvedSize < nSize)
+     {
+         FD_ZERO(&read_set);
+         FD_SET(socketFd, &read_set);
+
+         tmval.tv_sec = 10;
+         tmval.tv_usec = 0;
+
+         ret = select(socketFd + 1, &read_set, NULL, NULL, &tmval);
+         if(ret == 0) //超时
+         {
+             usleep(10 * 1000);
+             continue;
+         }
+
+         if(ret == -1)
+         {
+             return -1;
+         }
+
+         if(FD_ISSET(socketFd, &read_set))
+         {
+             ret = recv(socketFd, pBuffer + recvedSize, nSize - recvedSize, 0);
+             if(ret < 0  && (errno == EAGAIN || errno == EINTR))
+             {
+                 continue;
+             }
+
+             if(ret <= 0)
+             {
+                 return -3;
+             }
+
+             recvedSize += ret;
+         }
+     }
+}
+
+int set_socket_params(int sockfd)
+{
+    fcntl(sockfd, F_SETFL, 0, O_NONBLOCK);
+//    struct timeval tmval;
+//    tmval.tv_sec = 5;
+//    tmval.tv_usec = 0;
+//    int size = 5;
+
+//    setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF,  &size, sizeof (size));
+
+    return 0;
+}
+
+
+int process_client_command()
+{
+
+}
+
+int client_socketProcess(struct sockaddr_in * ipaddr)
+{
+    fd_set read_set;
+    int wait_count = 0;
+
+    set_socket_params(m_BindSocket);
+
+    struct timeval tmval;
+    tmval.tv_sec = 20;
+    tmval.tv_usec = 0;
+
+    FD_ZERO(&read_set);
+    FD_SET(m_BindSocket, &read_set);
+
+    while (wait_count < 10)
+    {
+        int ret = select(m_BindSocket + 1, &read_set, nullptr, nullptr, &tmval);
+        switch (ret)
+        {
+           case -1:
+            break;
+           case 0:
+            wait_count++;
+            break;
+        default:
+            if(FD_ISSET(m_BindSocket, &read_set))
+            {
+                //recv.....
+            }
+        }
+    }
+
+}
+
 void accept_process()
 {
     struct sockaddr_in cline_socket;
     socklen_t size;
-    pid_t pid;
     while(1)
     {
         size = sizeof(struct sockaddr_in);
